70564efb3b9ac9ea61dcf73e346cb736f1437369
dylvaz <vazquezadylan@gmail.com>
2020-11-01 18:36:07 -0800

    created connecton between server and mysql db

--- database/index.js
+++ database/index.js
@@ -2,3 +2,18 @@ const mysql = require('mysql');
 // https://www.npmjs.com/package/mysql
 
 // TODO: establish connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'foodDB'
+});
+
+db.connect();
+
+db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+module.exports = db;
